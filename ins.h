1398374b2a20f90319ddf9d1dde89b0e8771d6cc
9n8e <boombloxkewl@gmail.com>
2026-07-20 04:04:06 -0400

    argt implementation in instruction struct

--- ins.h
+++ ins.h
@@ -1,6 +1,9 @@
+#define MAX_OPS 3 // just in case i want to move from legacy instruction sets and implement modern extensions
+
 typedef struct {
 	void (*do)(void*);
 	int code;
 	int size;
+	arg_type ops[3]; // no legacy x86 instruction set allows for more than 3 operands provided for an instruction
 	char* name;
 } instruction;
