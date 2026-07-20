e95ae1ceebf7ebd54ce8a9abc68fb41eecd2f717
9n8e <boombloxkewl@gmail.com>
2026-07-20 04:34:09 -0400

    aspirations of vmtime

--- vmtime.c
+++ vmtime.c
@@ -1,1 +1,4 @@
 int vmTime() { return 1; }
+
+/* right now this looks stupid but the end goal is so that it will return the current time from an active counter, probably preserved on a separate thread, so that time can be measured between registers as well as stored elsewhere
+although i'm pretty sure if i bring this project out far enough a counter can be implemented implicitly within the registers via asm instructions */
